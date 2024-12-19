4f73e3b3593030e5274c7a9a328387237f99c57f
Eder Vieira de Oliveira Ribeiro <eder.edy@gmail.com>
2024-12-19 16:45:57 -0400

    Criação de usuario

--- blog/Models/Usuario.cs
+++ blog/Models/Usuario.cs
@@ -9,7 +9,7 @@ namespace Blog.Models
     {
 
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string? Name { get; set; }
         public string Email { get; set; }
         public string PasswordHash { get; set; }
         public string Bio { get; set; }
