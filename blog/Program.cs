8d4e912a01c36f196ba8589da3c8b1765a02a492
Eder Vieira <eder.edy@gmail.com>
2025-01-11 18:59:58 -0400

    Criado crud em dapper

--- blog/Program.cs
+++ blog/Program.cs
@@ -3,6 +3,7 @@ using Dapper.Contrib.Extensions;
 using Npgsql;
 using Dapper.FluentMap;
 using Blog.Mappings;
+using Dapper;
 
 FluentMapper.Initialize(config => 
 {
@@ -10,11 +11,15 @@ FluentMapper.Initialize(config =>
 });
 
 var connectionPost = new NpgsqlConnection(
-    connectionString: "User ID=postgres; Password=admin123; Server=localhost; Database=blog; Pooling=true; TrustServerCertificate=true;");
+    //connectionString: "User ID=postgres; Password=admin123; Server=localhost; Database=blog; Pooling=true; TrustServerCertificate=true;"
+    connectionString: "User ID=postgres; Password=3d3r3001; Server=localhost; Database=blog; Pooling=true; TrustServerCertificate=true;"
+);
 
 //ReadUsers(connectionPost);
 //ReadUser(connectionPost);
-CreateUser(connectionPost);
+//CreateUser(connectionPost);
+//UpdateUser(connectionPost);
+DeleteUser(connectionPost);
 
 static void ReadUsers(NpgsqlConnection connectionPost)
 {
@@ -39,20 +44,60 @@ static void ReadUser(NpgsqlConnection connectionPost)
 
 static void CreateUser(NpgsqlConnection connectionPost)
 {
-    var newUser = new Usuario
-    {
-        Bio = "Aqui estou eu",
-        Email = "aqui@teste",
-        Image = "https>//....",
-        Name = "Aluno de teste",
-        PasswordHash = "87sfds5gsfdg56",
-        Slug = "aluno-de-teste"
-    };
+    // var newUser = new Usuario
+    // {
+    //     Bio = "Aqui estou eu",
+    //     Email = "aqui@teste",
+    //     Image = "https>//....",
+    //     Name = "Aluno de teste",
+    //     PasswordHash = "87sfds5gsfdg56",
+    //     Slug = "aluno-de-teste"
+    // };
 
     using (var connection = new NpgsqlCommand())
     {
-        var users = connectionPost.Insert<Usuario>(newUser);
+        //var users = connectionPost.Insert<Usuario>(newUser);
 
-        Console.WriteLine($"Create: {users}");
+        connectionPost.Execute(@"INSERT INTO usuario (id, bio, email, image, name, password_hash, slug) VALUES (
+        @Id, @Bio, @Email, @Image, @Name, @Password, @Slug)",
+        new {
+            @Id = 1,
+            @Bio = "Aqui estou eu",
+            @Email = "aqui@teste",
+            @Image = "https>//....",
+            @Name = "Aluno de teste",
+            @Password = "87sfds5gsfdg56",
+            @Slug = "aluno-de-teste"
+        });
+
+        Console.WriteLine($"Usuario criado com sucesso!");
+    }
+}
+
+static void UpdateUser(NpgsqlConnection connectionPost)
+{
+    using (var connection = new NpgsqlCommand())
+    {
+        connectionPost.Execute(@"UPDATE usuario 
+                                 set bio = @Bio
+                                 where id = @Id",
+        new {
+            @Id = 1,
+            @Bio = "Aqui estou eu"
+        });
+
+        Console.WriteLine($"Usuario atualizado com sucesso!");
+    }
+}
+
+static void DeleteUser(NpgsqlConnection connectionPost)
+{
+    using (var connection = new NpgsqlCommand())
+    {
+        connectionPost.Execute(@"delete from usuario where id = @Id",new 
+        {
+            @Id = 1
+        });
+        Console.WriteLine($"Usuario deletado com sucesso!");
     }
 }
